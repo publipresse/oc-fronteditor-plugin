c3530eddcb93be7cf173a01e54ed84b5fb4e2ae4
PubliAlex <alexandre@publipresse.fr>
2023-11-10 11:06:31 +0100

    Allow multisite settings

--- models/TinyMCESetting.php
+++ models/TinyMCESetting.php
@@ -6,11 +6,11 @@ use Model;
 
 class TinyMCESetting extends \System\Models\SettingModel
 {
-    // use \October\Rain\Database\Traits\Multisite;
+    use \October\Rain\Database\Traits\Multisite;
 
     public $settingsCode = 'publipresse_fronteditor_tinymcesettings';
     public $settingsFields = 'fields.yaml';
 
-    // protected $propagatableSync = true;
-    // protected $propagatable = [];
+    protected $propagatableSync = true;
+    protected $propagatable = [];
 }
